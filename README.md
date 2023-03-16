beb0f25132e3f7e9640270735df7686a78f1e55a
erin-a-z <57600777+erin-a-z@users.noreply.github.com>
2023-03-15 22:44:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+The front end for Hyperloop Visison System.
+
+## Download instructions
+Download the directory
+
+Install all the dependencies
+`run pip -i requrements.txt`
+
+Run the flask app by using:
+`flask run`
---
+++ app-legacy.py
@@ -0,0 +1,24 @@
+import cv2
+from flask import Flask, render_template, Response
+app = Flask(__name__)
+cap = cv2.VideoCapture(0)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__ == '__main__':
+    app.run(debug=True)
