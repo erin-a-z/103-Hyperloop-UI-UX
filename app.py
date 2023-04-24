09bb28bcd622fd4d0d542ac3e239c0c3f4fc9795
erin-a-z <ezzeuu@gmail.com>
2023-04-24 17:06:14 -0400

    new app.py

--- app.py
+++ app.py
@@ -1,51 +1,39 @@
+from flask import Flask, request, Response, render_template
 import cv2
-from flask import Flask, render_template, Response, request
+from ml import analyze_frame
 
 app = Flask(__name__)
 
-# Load the machine learning model and any other necessary files here
-# ...
-
-# Define the function that will analyze the camera feed and return results
-def analyze_camera_feed():
-    # ... 
-    results = ''
-    return results
-
-# Define the function that will capture frames from the camera
-def gen_frames():
-    camera = cv2.VideoCapture(0)
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-# Define the route for the home page
+# This route accepts the image file and displays it on the webpage
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Define the route for the video feed
+# This route accepts the image file and returns it to the client in real-time
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(get_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# Define the route for the machine learning output
-@app.route('/output')
-def output():
-    results = analyze_camera_feed()
-    return render_template('output.html', results=results)
+# This function reads the frames from the video capture, analyzes them using the ml.analyze_frame function, and returns them as JPEG images
+def get_frames():
+    capture = cv2.VideoCapture(0)
 
-# Define the route for stopping the analyzed object
-@app.route('/stop', methods=['POST'])
-def stop():
-    # ...
-    return "Object stopped"
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            break
+
+        # analyze the frame using the ml.analyze_frame function
+        analyzed_frame = analyze_frame(frame)
+
+        # encode the frame in JPEG format
+        jpeg = cv2.imencode('.jpg', analyzed_frame)[1].tobytes()
+
+        # yield the result to the client
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n')
+
+    capture.release()
 
 if __name__ == '__main__':
     app.run(debug=True)
